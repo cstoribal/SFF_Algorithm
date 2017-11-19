5ffcfb3012943b45bebf2da631bd8df5b76fb7fd
cstoribal <cribal@crans.org>
2017-11-20 00:37:48 +0100

    up script

--- src/scripts/script_all.sh
+++ src/scripts/script_all.sh
@@ -1,10 +1,6 @@
 #!/bin/bash 
 ##include <stdio.h>
 
-# 1 default params
-
-
-
 LIST_FOLD=( "customseries/" "Semireal/semireal_mb4/" "Semireal/semireal_mb2/" "Semireal/semireal_mb1/" "Semireal/semireal_mb3/" "Simulations/simu4/" "Simulations/simu5/" "Simulations/s96serie/" "Simulations/")
 # LIST_FOLD=( "customseries/") 
 LIST_PARAMS=("./*.param.txt")
@@ -16,9 +12,6 @@ prog_sff="../../ProjSFF "$opt_param" -D "
 for fold in ${LIST_FOLD[*]}
   do
   subdir=${fold//\//_}
-  ##echo $subdir
-  ##mkdir $subdir
-  ##cd $subdir
   LIST_SAMP=("/media/sf_F_DRIVE/Stage/Samples/"$fold*.txt)
   for samp in ${LIST_SAMP[*]}
     do
@@ -26,7 +19,7 @@ for fold in ${LIST_FOLD[*]}
       do
       linecode=$prog_param""$param" -f "$samp
       echo $linecode
-      #eval $linecode
+      eval $linecode
       mkdir ${param%??????????}
       cd ${param%??????????}
       mkdir $subdir
@@ -34,7 +27,7 @@ for fold in ${LIST_FOLD[*]}
       
       linecode=$prog_sff""$samp
       echo $linecode
-      #eval $linecode
+      eval $linecode
       cd ..
       cd ..
     done
