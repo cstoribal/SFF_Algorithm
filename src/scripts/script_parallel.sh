e62b2780a58fe4b6c1dc2ceceb8306a1838ecb9a
cstoribal <stophe@crans.org>
2018-02-12 16:59:10 +0100

    v1.2 scripts

--- src/scripts/script_parallel.sh
+++ src/scripts/script_parallel.sh
@@ -1,54 +1,59 @@
 #!/bin/bash 
 ##include <stdio.h>
 
-
+#Only if usefull, for instance if this location is RAM
 cp ./SFFDataMgmt /data/tmp/
 cp ./ProjSFF     /data/tmp/
 
-convert_and_run() {
+
+### The actual program
+run_command() {
     prog_param="/data/tmp/SFFDataMgmt -M "
     prog_sff="/data/tmp/ProjSFF -optf "$2" -D "
-    #prog_param="./SFFDataMgmt -M "
-    #prog_sff="../../ProjSFF -optf "$2" -D "
-    for param in $1
-      do
-      linecode=$prog_param""$param" -f "$3
-      echo $linecode
-      eval $linecode
-      mkdir ${param%??????????}
-      cd ${param%??????????}
-      mkdir $4
-      cd $4
-      
-      linecode=$prog_sff""$3
-      echo $linecode
-      eval $linecode
-      cd ..
-      cd ..
-      cd ..
-    done
-}
 
+    linecode=$prog_param""$1" -f "$3
+    echo $linecode
+    eval $linecode
+    mkdir ${1%??????????} 
+    cd ${1%??????????}
+    mkdir $4
+    cd $4 #enter subfolders. Caution, it is actually 3 leveled-subfolder
+    
+    linecode=$prog_sff""$3
+    echo $linecode
+    eval $linecode
+    cd ..
+    cd ..
+    cd ..
+    
+}
+export -f run_command
+
+# Logging the output of each sample (all parameters)
+convert_and_run() {
+    sampname=${3##*/}
+    sampname=${sampname%?????????}
+    parallel -j 1 --resume-failed --joblog ./exp/log/$4/$sampname.log run_command ::: $1 ::: $2 ::: $3 ::: $4
+}
 export -f convert_and_run
 
 
 
 #LIST_FOLD=( "customseries/" "Semireal/semireal_mb4/" "Semireal/semireal_mb2/" "Semireal/semireal_mb1/" "Semireal/semireal_mb3/" "Simulations/simu4/" "Simulations/simu5/" "Simulations/s96serie/" "Simulations/")
-LIST_FOLD=( "Semireal/semireal_mb1/" "Semireal/semireal_mb4/")
-#LIST_FOLD=( "customseries/" )
+LIST_FOLD=( "Semireal/semireal_mb4/" "Semireal/semireal_mb1/")
 LIST_PARAMS=("./exp/*.param.txt")
 opt_param=" 0 "
 
 mkdir "./exp"
 mkdir "./exp/log"
 
-
 for fold in ${LIST_FOLD[*]}
   do
   subdir=${fold//\//_}
   subdir=${subdir%?}
   LIST_SAMP=("/data/data/Stage/Samples/"$fold*.txt)
-  parallel -j 8 --resume-failed --joblog ./exp/log/$subdir.log convert_and_run ::: "${LIST_PARAMS[*]}" ::: $opt_param ::: ${LIST_SAMP[*]} ::: $subdir
+  mkdir ./exp/log/$subdir
+  parallel -j 6 --resume-failed --joblog ./exp/log/$subdir.log convert_and_run ::: "${LIST_PARAMS[*]}" ::: $opt_param ::: ${LIST_SAMP[*]} ::: $subdir
 done
 
 cd ./exp
